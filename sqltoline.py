5aedde12614b28793fddc1185f89f627b16a1d9b
Panagiotis Papadimitriou <papadimitriou@odesk.com>
2013-08-27 16:12:32 -0700

    Made sql2line.py to work (a) with comments, and (b) multi-line scripts

--- sqltoline.py
+++ sqltoline.py
@@ -1,14 +1,22 @@
 #/usr/bin/env python
 import sys
 import re
+import logging
+import sqlparse
 
 if len(sys.argv) != 2:
     print 'Usage: ' + sys.argv[0] + ' <sql file>'
     sys.exit(1)
 
-sql = ' '.join(filter(lambda x: not x.startswith('--'), 
-               open(sys.argv[1], 'r').readlines()))
-
-print re.sub('\\s+', ' ', sql).replace('"', '\\"')
-
-
+logging.basicConfig(level=logging.INFO)
+sql = ''.join(open(sys.argv[1]).readlines())
+for i, statement in enumerate(sqlparse.split(sql)):
+    if not statement.strip():
+        continue
+    formatted = sqlparse.format(statement, 
+                                keyword_case='lower', 
+                                strip_comments=True,
+                                reindent=True)
+    print '\nStatement #%d' % (i + 1, )
+    logging.debug(formatted)
+    print re.sub('\\s+', ' ', formatted).replace('"', '\\"')
