9b2dc9335d9cdbdf9721d3fe86027dc4e3b9ecb7
Anna Lysak <ganna1024@gmail.com>
2016-05-12 11:13:49 +0300

    Fixed default value, set target variable required

--- cloudml/trainer/trainer.py
+++ cloudml/trainer/trainer.py
@@ -697,7 +697,7 @@ class Trainer(object):
         for feature_name, feature in self._feature_model.features.iteritems():
             ft = feature.get('type', None)
             item = row_data.get(feature_name, None)
-            if (item is None or item == '') and \
+            if is_empty(item) and \
                 self._feature_model.target_variable == feature_name and \
                     not is_predict:
                 raise ItemParseException('Target feature is null')
@@ -742,7 +742,7 @@ class Trainer(object):
                     feature.get('transformer') is not None:
                 result = TRANSFORMERS[feature['transformer-type']]['default']
             elif feature.get('type') is not None:
-                result = FEATURE_TYPE_DEFAULTS.get(feature['type'], value)
+                result = feature['type']._default_value
 
         return result
 
